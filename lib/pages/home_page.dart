dc4c66ee5acfd7c4669a3c3a032786f5df48834f
Guilherme Potter Petry <guilherme.potter@compasso.com.br>
2024-04-04 15:00:48 -0300

    feat: Implementado AppBar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,57 @@
 import 'package:flutter/material.dart';
-
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Container(
+        padding: const EdgeInsets.all(40),
+        child: Column(   
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Stack(
+                  children: [
+                    Positioned(
+                      left: 80,
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Color(0xffffbba8),
+                        ),
+                        child: const Text(
+                          textAlign: TextAlign.center,
+                          '9',
+                          style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xfffa8072),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Text(
+                      'News',
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.search),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
