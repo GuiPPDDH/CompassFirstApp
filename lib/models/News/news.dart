119b04b7d4188507c5cf90102e8f053d45092a63
Guilherme Potter Petry <guilherme.potter@compasso.com.br>
2024-04-11 10:39:45 -0300

    feature: adicionado novo atributo imagePath na model News

--- lib/models/News/news.dart
+++ lib/models/News/news.dart
@@ -1,16 +1,19 @@
 class News {
   final String title;
   final String description;
+  final String imagePath;
 
   News({
     required this.title,
     required this.description,
+    required this.imagePath,
   });
 
   factory News.fromJson(Map<String, dynamic> json) {
     return News(
       title: json['title'],
       description: json['description'],
+      imagePath: json[''],
     );
   }
 }
