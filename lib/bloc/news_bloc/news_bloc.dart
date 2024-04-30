b201694382b8303d631cc39885621ff46db494b4
Guilherme Potter Petry <guilherme.potter@compasso.com.br>
2024-04-30 08:31:34 -0300

    feature: Implementado try-catch com NewsStateError nos métodos de NewsBloc

--- lib/bloc/news_bloc/news_bloc.dart
+++ lib/bloc/news_bloc/news_bloc.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:developer';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../models/api_response/api_response.dart';
@@ -16,16 +17,34 @@ class NewsBloc extends Bloc<NewsEvent, NewsState> {
   }
 
   FutureOr<void> _fetchNews(FetchNews event, Emitter<NewsState> emitter) async {
-    emitter(NewsStateLoading());
-    final apiResponse = await _apiResponseRepository.fetchArticles();
-    emitter(NewsStateSuccess(apiResponse: apiResponse));
+    try {
+      emitter(NewsStateLoading());
+      final apiResponse = await _apiResponseRepository.fetchArticles();
+      emitter(NewsStateSuccess(apiResponse: apiResponse));
+    } catch (e, s) {
+      log(
+        'Falha ao buscar as notícias.',
+        error: e,
+        stackTrace: s,
+      );
+      emitter(NewsStateError(errorMessage: 'Falha ao buscar as notícias.'));
+    }
   }
 
   FutureOr<void> _searchNews(
       SearchNews event, Emitter<NewsState> emitter) async {
-    emitter(NewsStateLoading());
-    final apiResponse =
-        await _apiResponseRepository.searchArticles(event.query);
-    emitter(NewsStateSuccess(apiResponse: apiResponse));
+    try {
+      emitter(NewsStateLoading());
+      final apiResponse =
+          await _apiResponseRepository.searchArticles(event.query);
+      emitter(NewsStateSuccess(apiResponse: apiResponse));
+    } catch (e, s) {
+      log(
+        'Falha ao procurar as notícias.',
+        error: e,
+        stackTrace: s,
+      );
+      emitter(NewsStateError(errorMessage: 'Falha ao procurar a notícia.'));
+    }
   }
 }
